1ecb5521550edc9c7f9236045cb8418994bc21f8
Akarsh Goyal <akarshg14@gmail.com>
2018-09-02 23:57:44 +0530

    updated main.js

--- A1/main.js
+++ A1/main.js
@@ -6,10 +6,30 @@ function submitForm(){
 	let string = formValues.string.replace(/ /g, '+');
 	console.log(string);
 	const serverAddr = `http://127.0.0.1:8000/?string=${string}`;
-
+	var myNode = document.getElementById("images");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
 	fetch(serverAddr)
 		.then(e => e.json())
-		.then(console.log)
+		.then((obj) => {
+			let arr = Object.keys(obj);
+			arr.forEach((e) => {
+				let div = document.getElementById('images')
+				for (let i = 0; i< obj[e]; i++){
+					let di = document.createElement('div');
+					let elem = document.createElement('img');
+					elem.setAttribute("src", `images/${e}${i+1}.jpg`);
+					/*di.innerHTML = `${e}${i+1}`;*/
+					elem.setAttribute("height", "200");
+					elem.setAttribute("alt", "Flower");
+					elem.classList.add("foo");
+					di.appendChild(elem);
+					div.appendChild(di);
+					console.log(obj);	
+				}
+			})
+		})
 		.catch(console.error)
 }
 /*window.onload()*/
