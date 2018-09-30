b6d4351e5a56fadd14a93e69dcb28ade75aa098e
Mayank Sharma <msharma@iitk.ac.in>
2018-10-01 02:59:36 +0530

    Added Lab 3 files

--- MongoDBLab/index.php
+++ MongoDBLab/index.php
@@ -1,14 +1,49 @@
+<?php 
+	if (!empty($_POST['first'])){
+		echo "First";
+	}
+	if (!empty($_POST['second'])){
+		echo "Second";
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>New Tab</title>
+<title>CS252 Lab 4</title>
+<script>
+	function show(val){
+	val = val.id
+	if (val == 'first'){
+		elem = document.getElementById('one');
+		elem2 = document.getElementById('first');
+	elem.style.display = "";
+	elem2.style.display = "none";
+}
+else if (val == 'sec'){
+		elem = document.getElementById('two');
+		elem2 = document.getElementById('sec');
+	elem.style.display = "";
+	elem2.style.display = "none";
+}
+else if (val == 'third'){
+		elem = document.getElementById('three');
+		elem2 = document.getElementById('third');
+	elem.style.display = "";
+	elem2.style.display = "none";
+}
+	console.log(val);
+}	
+</script>
 </head>
 <body>
 </body>
 
 <h3>The Police Station with  slowest response time to an FIR:</h3><br>
 <?php
+	echo "<form onsubmit='return false' onsubmit='return false' ;
+		action='index.php/first'><button id='first' onclick='show(this)'>Click here to know</button></form>";
 require_once __DIR__ . "/vendor/autoload.php";
 $conn = (new MongoDB\Client)->cs252;
 $coll = $conn->fir;
@@ -74,6 +109,7 @@ foreach ($cursor as $document){
 	array_push($pol, $temp);
 }
 
+echo "<div id='one' style='display:none'>";
 $maxID = max_attribute_in_array($pol, 'timeDiff');
 $cursor = $coll->findOne(['_id' => $maxID]);
 echo "<b>FIR Number</b>:	", $cursor['FIR_REG_NUM'], "<br>"; 
@@ -83,7 +119,8 @@ echo "<b>Zone_Name</b>:	", $cursor['ZONE_NAME'], "<br>";
 echo "<b>Status</b>:	", $cursor['Status'], "<br>"; 
 echo "<b>Act_Section</b>:	"; 
 printArray($cursor['Act_Section']);
-echo "<br>"; 
+echo "<hr>"; 
+echo "</div>";
 
 $keyArr = array_keys($laws);
 $max = 0;
@@ -92,6 +129,17 @@ foreach($keyArr as $a){
 	if ($laws[$a] > $max){
 		$maxLaw = $a;
 		$max = $laws[$a];
+	}
+}
+$min = 10000;
+$minLaw = "";
+/*for ($i = 0; $i <= sizeof($keyArr); $i++){
+ *  [>echo $keyArr[$i], "\n";<]
+ *}*/
+foreach($keyArr as $a){
+	if ($laws[$a] < $min && $laws[$a] > 0){
+		$minLaw = $a;
+		$min = $laws[$a];
 	}
 }
 $keyArr = array_keys($districts);
@@ -103,9 +151,19 @@ foreach($keyArr as $a){
 		$max = $districts[$a];
 	}
 }
-echo "<h3>The Act Section which has been violated most is :</h3><br>";
-echo "<b>$maxLaw - Criminal Intimidation</b>";
+echo "<h3>Most and least uniquely applied FIRs</h3>";
+echo "<form onsubmit='return false' method='post' action='index.php/second'><button id='sec' onclick='show(this)'>Click here to know</button></form>";
+echo "<div id='two' style='display:none'>";
+	echo "<h3>The Act Section which has been violated most is :</h3><br>";
+	echo "<b>Section $maxLaw - Criminal Intimidation</b>";
+	echo "<h3>The Act Section which has been violated least is :</h3><br>";
+	echo "<b>Section $minLaw = $min number of times</b>";
+	echo "<hr>";
+echo "</div>";
 echo "<h3>The District with maximum number of FIRs registered: <br></h3>";
-echo "<b>$maxDist";
+echo "<div id='three' style='display:none'>";
+echo "<b>$maxDist = $max FIRs have been registered!";
+echo "</div>";
+echo "<button id='third' onclick='show(this)'>Click here to know</button>";
 ?>
 </html>
